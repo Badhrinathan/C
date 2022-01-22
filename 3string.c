7b2f029655d922fb3e581288fde95d22c4daed83
Badhrinathan <61671275+Badhrinathan@users.noreply.github.com>
2022-01-22 23:03:55 +0530

    Create 3string.c

---
+++ 3string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  char b[10]="hello dude"; //in "C" we use char data type as a string and mention size of the variable
+  printf("%s",b); //get output as a %s
+}
+/****
+%d - decimal integer
+%f - Floating point
+%lf - Double
+%e - Exponential
+%u - Unsigned
+%x - Hexadecimal
+%o - Octal
+%i - Single integer
+%c - Character
+%s - String
+****/
