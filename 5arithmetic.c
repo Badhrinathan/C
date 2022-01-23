ea6a110b6fdb42e2681c13df792c50c05af598d7
Badhrinathan <61671275+Badhrinathan@users.noreply.github.com>
2022-01-23 14:21:00 +0530

    Create 5arithmetic.c

---
+++ 5arithmetic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter value of a:");
+  scanf("%d", &a);
+  printf("Enter a value of b:");
+  scanf("%d", &b);
+  printf("%d\n", a+b); //add
+  printf("%d\n", a-b); //sub
+  printf("%d\n", a*b); //multiply
+  printf("%d\n", a/b); //divide
+  printf("%d\n", a%b); //modulus
+  return 0;
+}
+
+//https://gitub.com/Badhrinathan
