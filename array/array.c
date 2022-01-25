e8ebb3b615c2cfc63249dc68bbb8bbf77e900714
Badhrinathan <61671275+Badhrinathan@users.noreply.github.com>
2022-01-25 16:24:50 +0000

    Create array.c

---
+++ array/array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a[5]={10,11,12,13,14};
+    for(int i=0;i<=4;i++) //for loop
+    {
+        printf("\na[%d]=%d",i,a[i]); //array position and value
+    }
+ return 0;   
+}
