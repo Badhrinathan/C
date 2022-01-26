41fa8f946a2857e816d3fc038d65feeee63a487d
Badhrinathan <61671275+Badhrinathan@users.noreply.github.com>
2022-01-26 12:44:13 +0000

    Create 1if.c

---
+++ Control_statements/1if.c
@@ -0,0 +1,21 @@
+// elgible for vote
+#include<stdio.h>
+int main()
+{
+ int age;
+ printf("Enter your age:");
+ scanf("%d",&age);
+ if(age>=18)
+ printf("you can vote");
+ return 0;
+}
+/*
+a==b a is equal to b
+a!=b a is not equal to b
+a<b  a is less than b
+a>b  a is greater than b
+a<=b a is less than or equal to b
+a>=b a is greater than or equal  to b
+*/
+
+//https://github.com/Badhrinathan
