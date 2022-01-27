65112faa608affe191a4a4c64b6fa7d635a516dd
Badhrinathan <61671275+Badhrinathan@users.noreply.github.com>
2022-01-27 17:10:56 +0000

    Create while.c

---
+++ Loop/while.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+ int i=1; //intialisation
+ while(i<=10) //while(Condition)
+ {
+  printf("%d\n",i);
+  i++; //increment
+ }
+ return 0;
+}
+//NOTE: i++,i=i+1,++i are same
+
+/*
+output
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
+
+//https://github.com/Badhrinathan
